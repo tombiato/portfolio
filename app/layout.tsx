8bbf1609dd35919412c81a41eb61c85b68e5f01b
Tom Biato <biatotom@gmail.com>
2025-01-16 14:08:34 +0100

    Add utility functions, theme provider, and new components; update global styles and configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,33 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import './globals.css'
+import {ThemeProvider} from '@/components/theme-provider'
 
 const inter = Inter({
-  subsets: ["latin"],
-});
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "Tom's Portfolio",
-  description: "Awsome Mordern Next Portfolio",
-};
+  description: 'Awsome Mordern Next Portfolio',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Hero from '@/components/Hero'
+import {FloatingNav} from '@/components/ui/FloatingNav'
+import {FaHome} from 'react-icons/fa'
+
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <h1>Hello, Portfolio!</h1>
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <FloatingNav
+          className=""
+          navItems={[{name: 'Home', link: '/', icon: <FaHome />}]}
+        />
+        <Hero />
       </div>
     </main>
-  );
+  )
 }
