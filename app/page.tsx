031142ea0ca816d434a920feee0fcac997a03b0c
Tom Biato <biatotom@gmail.com>
2025-01-21 14:07:13 +0100

    Add Process component to showcase project approach with animated cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Clients from '@/components/Clients'
 import Experience from '@/components/Experience'
 import Grid from '@/components/Grid'
 import Hero from '@/components/Hero'
+import Process from '@/components/Process'
 import RecentProjects from '@/components/RecentProjects'
 import { FloatingNav } from '@/components/ui/FloatingNav'
 import { navItems } from '@/data'
@@ -16,6 +17,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Process />
       </div>
     </main>
   )
